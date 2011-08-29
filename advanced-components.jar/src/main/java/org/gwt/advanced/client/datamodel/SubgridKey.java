48246b147cdd9848d11c8bc2c124d46134e08d4f
skls79 <skls79@6c0c9b24-5345-0410-b1ed-ac54f45b8273>
2011-08-29 18:53:54 +0000

    2.0.7 started

--- advanced-components.jar/src/main/java/org/gwt/advanced/client/datamodel/SubgridKey.java
+++ advanced-components.jar/src/main/java/org/gwt/advanced/client/datamodel/SubgridKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Sergey Skladchikov
+ * Copyright 2011 Sergey Skladchikov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
