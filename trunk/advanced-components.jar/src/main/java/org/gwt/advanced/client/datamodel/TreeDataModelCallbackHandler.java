87d616b1e211dd4c59c8b5804bafa11ae9cadd8d
skls79 <skls79@6c0c9b24-5345-0410-b1ed-ac54f45b8273>
2013-09-16 12:45:57 +0000

    prepare for 2.0.9

--- trunk/advanced-components.jar/src/main/java/org/gwt/advanced/client/datamodel/TreeDataModelCallbackHandler.java
+++ trunk/advanced-components.jar/src/main/java/org/gwt/advanced/client/datamodel/TreeDataModelCallbackHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2012 Sergey Skladchikov
+ * Copyright 2008-2013 Sergey Skladchikov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
